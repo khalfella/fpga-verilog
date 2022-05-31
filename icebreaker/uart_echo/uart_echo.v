f515371e493766695c5b45ad400623716df3b9de
Mohamed Khalfella <khalfella@gmail.com>
2022-05-31 02:37:12 +0000

    Switched to use uart module as a wrapper

--- icebreaker/uart_echo/uart_echo.v
+++ icebreaker/uart_echo/uart_echo.v
@@ -39,62 +39,47 @@ module top (
 	  //.SCLK()
 	);
 
-
-	wire tx_clk;
-	wire rx_clk;
-
-	uart_baud_gen #(
-		.CLOCK_RATE(42000000), 
-		.BAUD_RATE(115200)
-	) uart_baud_gen_unit (
-		.clk(clk_42mhz),
-		.rst(1'b0),
-		.tx_clk(tx_clk),
-		.rx_clk(rx_clk),
-	);
-
 	reg uart_tx_start = 0;
 	reg [7:0] uart_tx_data = 0;
+	wire uart_tx_clk;
 	wire uart_tx_done;
 	wire uart_tx_busy;
-	uart_tx uart_tx_unit (
-		.clk(clk_42mhz), 
-		.tx_clk(tx_clk),
-		.enabled(1'b1),
-		.start(uart_tx_start),
-		.in(uart_tx_data),
 
-		.tx(TX),
-		.done(uart_tx_done),
-		.busy(uart_tx_busy)
-	);
-
-	wire [7:0] uart_rx_data;
+	wire uart_rx_clk;
+	reg [7:0] uart_rx_data;
 	wire uart_rx_err;
 	wire uart_rx_done;
 	wire uart_rx_busy;
-	uart_rx uart_rx_unit (
+	uart #(
+		.CLOCK_RATE(42000000),
+		.BAUD_RATE(115200)
+	) uart_unit (
 		.clk(clk_42mhz),
-		.rx_clk(rx_clk),
-		.enabled(1'b1),
-		.rx(RX),
-		.data(uart_rx_data),
-		.err(uart_rx_err),
-		.done(uart_rx_done),
-		.busy(uart_rx_busy)
-	);
+		.uart_rx_pin(RX),
+		.uart_tx_pin(TX),
+		.uart_tx_start(uart_tx_start),
+		.uart_tx_data(uart_tx_data),
+		.uart_tx_clk(uart_tx_clk),
+		.uart_tx_done(uart_tx_done),
+		.uart_tx_busy(uart_tx_busy),
 
+		.uart_rx_clk(uart_rx_clk),
+		.uart_rx_data(uart_rx_data),
+		.uart_rx_err(uart_rx_err),
+		.uart_rx_done(uart_rx_done),
+		.uart_rx_busy(uart_rx_busy)
+	);
 
 	assign LEDG_N = ~uart_rx_busy;
 	assign LEDR_N = ~uart_tx_busy;
 	always @(posedge clk_42mhz) begin
-		if (rx_clk) begin
+		if (uart_rx_clk) begin
 			if (uart_rx_done) begin
 				uart_tx_data <= uart_rx_data;
 				uart_tx_start <= 1;
 			end
 		end
-		if (tx_clk) begin
+		if (uart_tx_clk) begin
 			if (uart_tx_done) begin
 				uart_tx_start <= 0;
 			end
