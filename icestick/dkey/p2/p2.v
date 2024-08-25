3eba47336792765983e249fa59964184ac9fdad2
Mohamed Khalfella <khalfella@gmail.com>
2024-08-24 23:16:59 -0700

    Added solution to digi-key part2 challenge

---
+++ icestick/dkey/p2/p2.v
@@ -0,0 +1,17 @@
+`default_nettype none
+
+module top (
+	output wire	D1,
+	output wire	D2,
+	output wire	D3,
+	output wire	D4,
+	output wire	D5
+);
+
+	assign D1 = 1;
+	assign D2 = 1;
+	assign D3 = 1;
+	assign D4 = 1;
+	assign D5 = 0;
+
+endmodule
